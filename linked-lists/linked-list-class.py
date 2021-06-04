ecaaf4d70dcf2f92f5d8bdbee67aeecca3ebdef8
enPlace <n.place@protonmail.com>
2021-06-04 17:12:49 -0300

    practice

--- linked-lists/linked-list-class.py
+++ linked-lists/linked-list-class.py
@@ -1,5 +1,4 @@
-class linkedListNode: 
-    
+class linkedListNode:   
     def __init__(self, value, nextNode = None):
         self.value = value
         self.nextNode = nextNode
@@ -119,4 +118,65 @@ newll.deleteNode(4)
 newll.printList()
 newll.deleteNode(7)
 newll.printList()
+newll.insertNode("#",1)
+print(newll.head.value)
 
+
+
+#_______below is the structure as written by the google/python data structures class in udacity 
+#https://classroom.udacity.com/courses/ud513/lessons/7117335401/concepts/78875247320923
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def get_position(self, position):
+        counter = 1
+        current = self.head
+        if position < 1:
+            return None
+        while current and counter <= position:
+            if counter == position:
+                return current
+            current = current.next
+            counter += 1
+        return None
+
+    def insert(self, new_element, position):
+        counter = 1
+        current = self.head
+        if position > 1:
+            while current and counter < position:
+                if counter == position - 1:
+                    new_element.next = current.next
+                    current.next = new_element
+                current = current.next
+                counter += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
+
+    def delete(self, value):
+        current = self.head
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
