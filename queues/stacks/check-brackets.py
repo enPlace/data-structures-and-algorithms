172194b247b370f4dfc492c7278f4e2851937d2a
enPlace <n.place@protonmail.com>
2021-06-05 19:14:24 -0300

    bal bracks

---
+++ queues/stacks/check-brackets.py
@@ -0,0 +1,30 @@
+def isBalanced(s):
+    # Write your code here
+    stack = []
+    brackets = {"}":"{", "]":"[", ")":"("}
+
+    for i in s: 
+        if i in brackets.values(): 
+            stack.append(i)
+        elif stack: 
+            top = stack.pop()
+            if brackets[i]!=top:
+                return "NO"
+        else: 
+            return "NO"
+    return "NO" if stack else "YES"
+
+
+
+
+print(isBalanced("()")) #YES
+print(isBalanced(")("))#NO
+print(isBalanced("({[]})"))#YES
+print(isBalanced("{]"))#NO
+print(isBalanced("}"))#NO
+print(isBalanced("()()()()()()()()"))#YES
+print(isBalanced("{[({([])})]}"))#YES
+print("______________")
+
+
+print(isBalanced("}[(]}[][)({]([][)}[)[]))({(]}{}]"))#NO
