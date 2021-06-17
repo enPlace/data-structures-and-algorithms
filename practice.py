b2bcf14f0fe35511767e3749f179b4cfd1a7090a
enPlace <n.place@protonmail.com>
2021-06-17 16:11:31 -0300

    practicing

--- practice.py
+++ practice.py
@@ -1,11 +1,12 @@
-def bubble_sort(arr):
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-1-i):
-            if arr[j]>arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j-1]>arr[j] and j>0:
+            arr[j-1], arr[j] = arr[j], arr[j-1]
+            j = j-1
+
+    print(arr)
 
 
-
-arr1 = [4,3,2,5,1]
-bubble_sort(arr1)
-print(arr1)
+arr1 = [4,3,2,7,6,10,20,8,1]
+insertion_sort(arr1)
